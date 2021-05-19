343bc36f3c298feaab702481be5b207425e644e9
ABskr <bskr26@gmx.de>
2021-05-19 14:04:07 +0200

    another commit

--- src/components/JobCard.jsx
+++ src/components/JobCard.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Button, Card } from 'react-bootstrap'
-import {withRouter} from'react-router-dom'
+import { withRouter } from 'react-router-dom'
 
 function JobCard({history, jobDesc, submitJobId}) {
   return (
-    <Card>
-      <Card.Header>{jobDesc.title}</Card.Header>
+    <Card className='mb-3' >
+      <Card.Header>{jobDesc.company}</Card.Header>
       <Card.Body>
-        <Card.Title>{jobDesc.company}</Card.Title>
+        <Card.Title>{jobDesc.title}</Card.Title>
         {/* <div
           dangerouslySetInnerHTML={{ __html: jobDesc.description }}
         /> */}
